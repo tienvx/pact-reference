d7489b8d5daeff497ee50d7e8bf3a2ecd85cb0d9
Ronald Holshausen <ronald.holshausen@smartbear.com>
2024-06-12 11:21:28 +1000

    chore(FFI): Add a test for mock server logs from C example

--- rust/pact_ffi/examples/logging/src/main.c
+++ rust/pact_ffi/examples/logging/src/main.c
@@ -2,6 +2,11 @@
 #include "pact.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#ifdef CURL
+#include <curl/curl.h>
+#endif
 
 #define ERROR_MSG_LEN 256
 
@@ -67,7 +72,7 @@ int main(void) {
     pactffi_log_message("example C", "error", "This is an error message");
     pactffi_log_message("example C", "trace", "This is a trace message");
 
-    char *logs = pactffi_fetch_log_buffer(NULL);
+    const char *logs = pactffi_fetch_log_buffer(NULL);
     if (logs == NULL) {
         printf("Could not get the buffered logs\n");
         return EXIT_FAILURE;
@@ -76,7 +81,47 @@ int main(void) {
     printf("---- Logs from buffer ----\n");
     printf("%s", logs);
     printf("--------------------------\n");
+
+    int len = strlen(logs);
+    if (len == 0) {
+        printf("Buffered logs are empty\n");
+        return EXIT_FAILURE;
+    }
     pactffi_string_delete(logs);
+
+    /**
+    * Test the logs from the mock server
+    */
+    #ifdef CURL
+        PactHandle pact = pactffi_new_pact("logging-test", "logging-test");
+        int port = pactffi_create_mock_server_for_transport(pact, "127.0.0.1", 0, "http", NULL);
+
+        CURL *curl = curl_easy_init();
+        if (curl) {
+            char url[32];
+            sprintf(url, "http://localhost:%d/", port);
+            printf("Executing request against %s\n", url);
+            curl_easy_setopt(curl, CURLOPT_URL, url);
+
+            CURLcode res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+
+            const char *mockserver_logs =  pactffi_mock_server_logs(port);
+            printf("---- Logs from mock server ----\n");
+            printf("%s", mockserver_logs);
+            printf("--------------------------\n");
+
+            int mockserver_logs_len = strlen(mockserver_logs);
+            pactffi_cleanup_mock_server(port);
+            if (mockserver_logs_len == 0) {
+                printf("Mock server logs are empty\n");
+                return EXIT_FAILURE;
+            }
+        } else {
+            printf("CURL is not available\n");
+            return EXIT_FAILURE;
+        }
+    #endif
 
     return EXIT_SUCCESS;
 }
