2b5b0c1eb54dacd6ce0e88c986609013fd0b7f2d
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-10-26 11:52:26 +0700

    docs: Add known log sink type

--- rust/pact_ffi/src/log/ffi.rs
+++ rust/pact_ffi/src/log/ffi.rs
@@ -215,7 +215,7 @@ pub extern "C" fn pactffi_logger_init() {
 /// - `-1`: Can't set logger (applying the logger failed, perhaps because one is applied already).
 /// - `-2`: No logger has been initialized (call `pactffi_logger_init` before any other log function).
 /// - `-3`: The sink specifier was not UTF-8 encoded.
-/// - `-4`: The sink type specified is not a known type (known types: "stdout", "stderr", or "file /some/path").
+/// - `-4`: The sink type specified is not a known type (known types: "stdout", "stderr", "buffer", or "file /some/path").
 /// - `-5`: No file path was specified in a file-type sink specification.
 /// - `-6`: Opening a sink to the specified file path failed (check permissions).
 ///
